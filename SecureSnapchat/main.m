890ea499cad8c3ac2509f77110b35e25728c9b8d
John Reynolds <reynoldsjk1@gmail.com>
2013-12-02 01:50:20 -0500

    Progress on encryption (still need to zip) Now prompts user for password in prep for decryption

--- SecureSnapchat/main.m
+++ SecureSnapchat/main.m
@@ -27,12 +27,82 @@ int main(int argc, char *argv[])
         
         char data[20];
         for (int x=0;x<20;data[x++] = (char)('A' + (arc4random_uniform(26))));
-        NSString *pass = [[NSString alloc] initWithBytes:data length:20 encoding:NSUTF8StringEncoding];
-//        NSString *passOutCommand = [@"-passout pass:" stringByAppendingString:pass];
-//        NSString *passInCommand = [@"-passin pass:" stringByAppendingString:pass];
-//        NSString *path = @"/usr/bin/openssl";
         
         
+        NSString *pass;// = [[NSString alloc] initWithBytes:data length:20 encoding:NSUTF8StringEncoding];
+        
+        
+        //get password
+        NSString *prompt = @"Please choose your password...";
+        NSString *defaultValue = @"";
+
+        NSAlert *alert = [NSAlert alertWithMessageText:prompt
+                                         defaultButton:@"Enter"
+                                       alternateButton:@"Cancel"
+                                           otherButton:nil
+                             informativeTextWithFormat:@"Please enter your password..."];
+
+        NSSecureTextField *input = [[NSSecureTextField alloc] initWithFrame:NSMakeRect(0, 0, 200, 24)];
+        [input setStringValue:defaultValue];
+        [alert setAccessoryView:input];
+    
+        
+        BOOL done = NO;
+        
+        while (!done) {
+            NSInteger button = [alert runModal];
+            
+            // affirmative closing ("ok")
+            if (button == NSAlertDefaultReturn) {
+                [input validateEditing];
+                
+                pass = [input stringValue];
+                
+            } else if (button == NSAlertAlternateReturn) {
+                //        NSLog(@"User cancelled");
+                exit(1);
+            } else {
+                //        NSLog(@"HUH?");
+            }
+            if (![pass isEqualToString:@""]) {
+                done = YES;
+            }
+            else NSLog(@"null password. do it again");
+        }
+        
+        //ReEnter Password
+        alert = [NSAlert alertWithMessageText:prompt
+                                 defaultButton:@"ReEnter"
+                               alternateButton:@"Cancel"
+                                   otherButton:nil
+                     informativeTextWithFormat:@"Please ReEnter your password..."];
+        
+        input = [[NSSecureTextField alloc] initWithFrame:NSMakeRect(0, 0, 200, 24)];
+        [input setStringValue:defaultValue];
+        [alert setAccessoryView:input];
+        NSInteger button = [alert runModal];
+        
+        // affirmative closing ("ok")
+        if (button == NSAlertDefaultReturn) {
+            [input validateEditing];
+            
+            if (![[input stringValue] isEqualToString:pass]) {
+                exit(2);
+            }
+            
+            pass = [input stringValue];
+            
+        } else if (button == NSAlertAlternateReturn) {
+            //        NSLog(@"User cancelled");
+            exit(1);
+        } else {
+            //        NSLog(@"HUH?");
+        }
+        if (![pass isEqualToString:@""]) {
+            done = YES;
+        }
+        
+        //set keys into place
         pid_t pid = fork();
         int status;
         if (pid == 0) {
